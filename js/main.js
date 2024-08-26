7597da7d8df459457e3db356ebd960733106da49
Andravi <andravi75@gmail.com>
2024-08-26 09:25:49 -0300

    swiper slider working

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-const swiper = new Swiper('.swiper-container', {
-        // Opções do Swiper
-        loop: true, // Habilita o loop infinito
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        autoplay: {
-            delay: 5000, // 5 segundos entre as transições
-        },
-    });
+console.log("3")
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
