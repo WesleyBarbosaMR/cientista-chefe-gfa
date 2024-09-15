65c090f0c37f573c29dd7b8c2ec3d5514ba20e97
Andravi <andravi75@gmail.com>
2024-09-15 16:19:07 -0300

    CCGFA-18 logo at header navbar implemented

--- js/header.js
+++ js/header.js
@@ -1,27 +1,6 @@
-let header = document.querySelector(".header");
+let header = document.querySelector(".headedr");
 
 header.innerHTML = `
     
-    <nav class="navbar navbar-expand-xl justify-content-end container">
-        <button
-          class="navbar-toggler me-3"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse col-lg-5" id="navbarNav">
-          <div class="container navbar-nav flex-grow-1 justify-content-end">
-            
-            <a class="btn" href="/index.html">Home</a>
-            <a class="btn" href="/pages/sobre.html">Sobre o Projeto</a>
-            
-            <a class="btn" href="#rodape-contato">Contato</a>
-          </div>
-        </div>
-      </nav>
+    
     `;
